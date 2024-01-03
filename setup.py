7a143e854ec0ab02744b357cec39efa95f9db722
lennylessizmor <lenny.lessizmor@gmx.co.uk>
2024-01-03 21:50:37 +0000

    updated setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from version import __version__
 setup(
     name='caxcli',
     version=__version__,
-    py_modules=['cli'],
+    py_modules=['caxcli'],
     install_requires=[
         'docopt>=0.6.2',
         'pandas',
